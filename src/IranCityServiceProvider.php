c52ba6de8433999c1966ec354906064abb511b87
saman sh <saman.sh999@gmail.com>
2019-01-15 11:42:34 +0330

    VueJs file complated

--- src/IranCityServiceProvider.php
+++ src/IranCityServiceProvider.php
@@ -8,16 +8,35 @@ class IranCityServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-
-        $this->publishes([
-            __DIR__.'/database/seeds' => database_path('seeds'),
-        ], 'samansh999-seeders');
+        $this->loadDependencies()
+            ->publishDependencies();
     }
 
     public function register()
     {
         //
+    }
+
+    private function loadDependencies()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+
+        return $this;
+    }
+
+    private function publishDependencies()
+    {
+        $this->publishes([
+            __DIR__ . '/database/migrations' => database_path('migrations'),
+        ], 'iran-city-migrations');
+
+        $this->publishes([
+            __DIR__ . '/database/seeds' => database_path('seeds'),
+        ], 'iran-city-seeds');
+
+        $this->publishes([
+            __DIR__ . '/resources/js' => resource_path('js'),
+        ], 'iran-city-vue-js');
     }
 }
