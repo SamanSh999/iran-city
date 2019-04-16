49444a09094e50bfae00af0c0f5195cc3f460ee1
saman sh <saman.sh999@gmail.com>
2019-04-16 11:24:34 +0430

    Add new vue component

---
+++ src/resources/js/components/samansh999_Iran_city/IranCityComponent2.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="container">
+
+        <div class="form-group row">
+            <label for="province" class="control-label text-center col-md-3"><span>استان</span><span class="text-danger"> * </span></label>
+            <div class="col-md-9">
+                <select class="form-control custom-select" name="province" id="province" v-model="province_selected"
+                        dir="rtl">
+                    <option v-for="province in provinces" v-bind:value="province.id" v-text="province.name"></option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row" v-if="province_selected">
+            <label for="cities" class="control-label text-center col-md-3"><span>شهرستان</span><span class="text-danger"> * </span></label>
+            <div class="col-md-9">
+                <select class="form-control" name="cities" id="cities" v-model="city_selected"
+                        dir="rtl">
+                    <option v-for="city in cities" v-bind:value="city.id" v-text="city.name"></option>
+                </select>
+            </div>
+            <p v-if="city_waiting" class="float-right text-success text-right" v-text="city_message" dir="rtl"></p>
+        </div>
+
+        <div class="form-group row" v-if="city_selected">
+            <label for="sections" class="control-label text-center col-md-3"><span>بخش</span><span class="text-danger"> * </span></label>
+            <div class="col-md-9">
+                <select class="form-control" name="sections" id="sections" v-model="section_selected"
+                        dir="rtl">
+                    <option v-for="section in sections" v-bind:value="section.id" v-text="section.name"></option>
+                </select>
+            </div>
+            <p v-if="section_waiting" class="float-right text-success text-right" v-text="section_message"
+               dir="rtl"></p>
+
+        </div>
+
+
+        <div class="form-group row" v-if="section_selected">
+            <label for="towns" class="control-label text-center col-md-3"><span>شهر</span><span class="text-danger">  </span></label>
+            <div class="col-md-9">
+                <select class="form-control" name="towns" id="towns" v-model="town_selected"
+                        dir="rtl">
+                    <option v-for="town in towns" v-bind:value="town.id" v-text="town.name"></option>
+                </select>
+            </div>
+            <p class=" text-success text-right" dir="rtl">از بین شهر یا دهستان یکی را باید انتخاب کنید</p>
+            <p v-if="town_waiting" class="float-right text-success text-right" v-text="town_message" dir="rtl"></p>
+
+        </div>
+
+        <div class="form-group row" v-if="section_selected">
+            <label for="rurals" class="control-label text-center col-md-3"><span>دهستان</span><span class="text-danger">  </span></label>
+            <div class="col-md-9">
+                <select class="form-control" name="rurals" id="rurals" v-model="rural_selected"
+                        dir="rtl">
+                    <option v-for="rural in rurals" v-bind:value="rural.id" v-text="rural.name"></option>
+                </select>
+            </div>
+            <p class=" text-success text-right" dir="rtl">از بین شهر یا دهستان یکی را باید انتخاب کنید</p>
+
+            <p v-if="rural_waiting" class="float-right text-success text-right" v-text="rural_message" dir="rtl"></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        //todo :: receive props and find city detail
+        props: ['province_id', 'city_id', 'section_id', 'town_id', 'rural_id'],
+        data: function () {
+            this.provinces = [];
+            axios.get('/iran-city/all-provinces').then(response => {
+                this.provinces = response.data;
+            });
+
+            return {
+                provinces: [],
+                province_selected: '',
+                province_waiting: false,
+                province_message: '',
+
+                cities: [],
+                city_selected: '',
+                city_waiting: false,
+                city_message: '',
+
+                sections: [],
+                section_selected: '',
+                section_waiting: false,
+                section_message: '',
+
+                towns: [],
+                town_selected: '',
+                town_waiting: false,
+                town_message: '',
+
+                rurals: [],
+                rural_selected: '',
+                rural_waiting: false,
+                rural_message: '',
+            };
+
+        },
+        watch: {
+            province_selected: function () {
+                this.cities = [];
+                this.city_selected = '';
+                this.city_waiting = true;
+                this.city_message = ' لطفا صبر کنید ... ';
+
+                this.sections = [];
+                this.section_selected = '';
+
+                this.getProvinceCities()
+            },
+            city_selected: function () {
+                if (this.city_selected !== '') {
+                    this.sections = [];
+                    this.section_selected = '';
+                    this.section_waiting = true;
+                    this.section_message = ' لطفا صبر کنید ... ';
+
+                    this.getCitySections()
+                }
+
+            },
+            section_selected: function () {
+                if (this.section_selected !== '') {
+                    this.towns = [];
+                    this.town_selected = '';
+                    this.town_waiting = true;
+                    this.town_message = ' لطفا صبر کنید ... ';
+
+                    this.rurals = [];
+                    this.rural_selected = '';
+                    this.rural_waiting = true;
+                    this.rural_message = ' لطفا صبر کنید ... ';
+
+                    this.getSectionTowns();
+                    this.getSectionRurals();
+                }
+
+            },
+            rural_selected: function () {
+                this.town_selected = '';
+            },
+            town_selected: function () {
+                this.rural_selected = '';
+            },
+        },
+        methods: {
+            getProvinceCities() {
+                axios.get('/iran-city/province-cities/' + this.province_selected).then(response => {
+                    this.cities = response.data;
+                    this.city_waiting = false;
+                }).catch(error => {
+                    //todo :: create server message
+                    this.city_message = 'مشکلی پیش امده با پشتیبانی تماس بگیرید';
+                    console.log(error)
+                });
+            },
+            getCitySections() {
+                axios.get('/iran-city/city-sections/' + this.city_selected).then(response => {
+                    this.sections = response.data;
+                    this.section_waiting = false;
+                }).catch(error => {
+                    //todo :: create server message
+                    this.section_message = 'مشکلی پیش امده با پشتیبانی تماس بگیرید';
+                });
+            },
+            getSectionTowns() {
+                axios.get('/iran-city/section-towns/' + this.section_selected).then(response => {
+                    this.towns = response.data;
+                    this.town_waiting = false;
+                }).catch(error => {
+                    //todo :: create server message
+                    this.section_message = 'مشکلی پیش امده با پشتیبانی تماس بگیرید';
+                });
+            },
+            getSectionRurals() {
+                axios.get('/iran-city/section-rurals/' + this.section_selected).then(response => {
+                    this.rurals = response.data;
+                    this.rural_waiting = false;
+                }).catch(error => {
+                    //todo :: create server message
+                    this.section_message = 'مشکلی پیش امده با پشتیبانی تماس بگیرید';
+                });
+            },
+        }
+
+    }
+</script>
+
